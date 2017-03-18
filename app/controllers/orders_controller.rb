7c2b2e949424d21b3b045d12a1a560872bdfc788
Eric-Guo <guochunzhong@bayekeji.com>
2017-03-18 15:02:35 +0800

    New order controller.

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,8 @@
+class OrdersController < ApplicationController
+  def create
+    @teddy = Teddy.find(params[:teddy_id])
+    order  = Order.create!(teddy_sku: @teddy.sku, amount: @teddy.price, state: 'pending')
+
+    redirect_to new_order_payment_path(order)
+  end
+end
