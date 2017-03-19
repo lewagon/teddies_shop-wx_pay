4e4de2674d93d404018a427615fb66b60a900463
Eric-Guo <guochunzhong@bayekeji.com>
2017-03-20 00:59:30 +0800

    Change to order paid page after user paid.

--- app/assets/javascripts/channels/notification.js
+++ app/assets/javascripts/channels/notification.js
@@ -2,7 +2,11 @@
   App.room = App.cable.subscriptions.create("NotificationChannel", {
     received: function(data) {
       console.log(data);
+      switch(data["shop_event"]) {
+        case "order_paid":
+          window.location.href = "/orders/" + data["order_id"];
+          break;
+      }
     }
   });
 }).call(this);
-
