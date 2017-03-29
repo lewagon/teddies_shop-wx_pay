988a3f94b56f0a4255660b1feb94878d35f739b9
Eric-Guo <guochunzhong@bayekeji.com>
2017-03-29 23:15:34 +0800

    Fix 401 Unauthorized

--- app/controllers/wechats_controller.rb
+++ app/controllers/wechats_controller.rb
@@ -1,4 +1,4 @@
-class WechatsController < ApplicationController
+class WechatsController < ActionController::Base
   # For details on the DSL available within this file, see https://github.com/Eric-Guo/wechat#rails-responder-controller-dsl
   wechat_responder
 
