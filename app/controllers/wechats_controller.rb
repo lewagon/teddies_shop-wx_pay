a72cc4db50a1bd3ef822609808994591ff7b7828
Eric-Guo <guochunzhong@bayekeji.com>
2017-03-30 14:55:16 +0800

    Add initial menu entry.

--- app/controllers/wechats_controller.rb
+++ app/controllers/wechats_controller.rb
@@ -2,6 +2,14 @@ class WechatsController < ActionController::Base
   # For details on the DSL available within this file, see https://github.com/Eric-Guo/wechat#rails-responder-controller-dsl
   wechat_responder
 
+  on :click, with: 'BTN_COUPON' do |request, key|
+    request.reply.text "User #{request[:FromUserName]} EventKey #{key} happen"
+  end
+
+  on :click, with: 'BTN_PROMOTION' do |request, key|
+    request.reply.text "User #{request[:FromUserName]} EventKey #{key} happen"
+  end
+
   on :text do |request, content|
     request.reply.text "echo: #{content}" # Just echo
   end
