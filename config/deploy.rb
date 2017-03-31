31bb380d17f7aed53f0ede55b28994675d1103e8
Eric-Guo <guochunzhong@bayekeji.com>
2017-03-31 11:12:43 +0800

    Link application.yml as whole settings.

--- config/deploy.rb
+++ config/deploy.rb
@@ -22,7 +22,7 @@ set :repo_url, 'git@github.com:Eric-Guo/teddies_shop-wx_pay.git'
 # set :pty, true
 
 # Default value for :linked_files is []
-# append :linked_files, "config/database.yml", "config/secrets.yml"
+append :linked_files, 'config/application.yml'
 
 # Default value for linked_dirs is []
 append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system'
