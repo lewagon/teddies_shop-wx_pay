8169f7953e1552f8db144743874d6cb645110880
Eric-Guo <guochunzhong@bayekeji.com>
2017-04-02 01:57:03 +0800

    Allow direct pay

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
     resource :payment, only: [:show]
   end
   resources :wewebs, only: [:index, :show]
+  get 'direct_pay', to: 'wewebs#pay'
+
   post 'wx_notify', to: 'payments#create'
   resource :wechat, only: [:show, :create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
